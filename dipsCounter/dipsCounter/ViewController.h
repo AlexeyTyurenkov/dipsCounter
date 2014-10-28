a21c76d7c6823de66721a0515a2d57fcf1d3e047
Oleksii Tiurenkov <alterego4@gmail.com>
2014-10-28 20:41:45 +0200

    - initial commit

---
+++ dipsCounter/dipsCounter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  dipsCounter
+//
+//  Created by Aleksey Tyurenkov on 10/28/14.
+//  Copyright (c) 2014 com.epam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ dipsCounter/dipsCounter/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  dipsCounter
+//
+//  Created by Aleksey Tyurenkov on 10/28/14.
+//  Copyright (c) 2014 com.epam. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
